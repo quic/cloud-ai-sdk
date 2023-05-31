357b7bebf4eb05845887254fd24901c070d14d75
Tangade, Shashi <stangade@qti.qualcomm.com>
2023-05-31 12:34:49 -0700

    Update model-preparator.sh
    Updated model prep WORKSPACE

--- tutorials/getting-started/NLP/bert/quickstart-bert-base/model-preparator.sh
+++ tutorials/getting-started/NLP/bert/quickstart-bert-base/model-preparator.sh
@@ -14,7 +14,7 @@ prepare_model(){
 	fi
 
 	python3 /opt/qti-aic/tools/qaic-pytools/qaic-model-preparator.py --config bertbase.yaml
-	python3 generateModel.py --model-ip-path  WORKSPACE/model_preparator_aic100.onnx --model-op-path ${model_output_path}
+	python3 generateModel.py --model-ip-path  WORKSPACE/bert-base-cased_fix_outofrange_fp16_preparator_aic100.onnx --model-op-path ${model_output_path}
 
 }
 
