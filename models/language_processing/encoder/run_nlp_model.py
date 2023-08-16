11532a60b62b664aa08723af472c0165535ff63f
Hosseini, Morteza <morteza@qti.qualcomm.com>
2023-08-16 14:05:59 -0700

    Update run_nlp_model.py

--- models/language_processing/encoder/run_nlp_model.py
+++ models/language_processing/encoder/run_nlp_model.py
@@ -84,7 +84,7 @@ def execute(cmd_elements, write_to_file, mode):
 
 # checks device status and cores available
 def check_device(DEVICE_ID, CORES, INSTANCES):
-    QAIC_UTIL = subprocess.run(f"/opt/qti-aic/tools/qaic-util -d {DEVICE_ID} -q",  shell=True, capture_output=True, text=True).stdout
+    QAIC_UTIL = subprocess.run(f"sudo /opt/qti-aic/tools/qaic-util -d {DEVICE_ID} -q",  shell=True, capture_output=True, text=True).stdout
     try:
         NSP_TOTAL = int(QAIC_UTIL.split("Nsp Total:")[1].split()[0])
     except:
@@ -329,7 +329,7 @@ def main(args):
     # Run for fp16
     run_output_dir = f"{OUTPUT_FOLDER}fp16-{MOTIF}"
     os.makedirs(run_output_dir, exist_ok=True)
-    cmd_elements = ["/opt/qti-aic/exec/qaic-runner",
+    cmd_elements = ["sudo", "/opt/qti-aic/exec/qaic-runner",
                     "-t", f"./compiled-bin-fp16-{MOTIF}",
                     "-a", f"{INSTANCES}",
                     "--time", f"{TIME}",
