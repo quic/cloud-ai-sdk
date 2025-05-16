799f1586e72331105d9e214c45ce61ee6ff38840
Zack McKevitt <zmckevit@qti.qualcomm.com>
2025-05-16 10:29:39 -0700

    Update copyright markings to the modern policy
    Move to using a yearless, Qualcomm Technologies Inc copyright
    instead of the existing dated Qualcomm Innovation Center copyrights.
    
    Signed-off-by: Zack McKevitt <zmckevit@qti.qualcomm.com>

--- models/multimodal/text_to_image/stable_diffusion/compile_models.sh
+++ models/multimodal/text_to_image/stable_diffusion/compile_models.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ####################################################################################################
-# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 ####################################################################################################
 
