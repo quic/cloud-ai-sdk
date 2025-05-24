80b9773e920e0fcd0c8caaeac026e04ccaffb9ee
Robert Morrison <rmorriso@qti.qualcomm.com>
2025-05-23 18:28:46 -0700

    Merge 21dfa5b from cloud-ai-sdk
    Update copyright markings to the modern policy
    
    Move to using a yearless, Qualcomm Technologies Inc copyright
    instead of the existing dated Qualcomm Innovation Center copyrights
    
    Original author: Zack McKevitt <zmckevit@qti.qualcomm.com>
    
    Signed-off-by: Robert Morrison <rmorriso@qti.qualcomm.com>

--- models/multimodal/text_to_image/sdxl_turbo/utils.py
+++ models/multimodal/text_to_image/sdxl_turbo/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
 import os
